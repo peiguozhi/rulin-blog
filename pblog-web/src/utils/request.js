6ea96c012c7aa4a1ab7f726edaa9ba292128eb3c
codescholar <1055826438@qq.com>
2023-08-23 22:52:39 +0800

    ajax  添加加载动画

--- pblog-web/src/utils/request.js
+++ pblog-web/src/utils/request.js
@@ -10,6 +10,7 @@ const service = axios.create({
 // request interceptor
 service.interceptors.request.use(
     config => {
+      window.vm.$bus.$emit('showLoading');
         // do something before request is sent
         let token = store.state.token
         if (token) {
@@ -40,6 +41,7 @@ service.interceptors.response.use(
      * You can also judge the status by HTTP Status Code
      */
     response => {
+      window.vm.$bus.$emit('hideLoading');
         const res = response.data
         // store.commit('SET_LOADING',false);
         // if the custom code is not 20000, it is judged as an error.
