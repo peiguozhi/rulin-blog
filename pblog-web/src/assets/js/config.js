74c7f4dfd547eed557df59b92028fd399ea3aa27
codescholar <1055826438@qq.com>
2023-08-23 22:48:05 +0800

    no message

--- pblog-web/src/assets/js/config.js
+++ pblog-web/src/assets/js/config.js
@@ -1,5 +1,5 @@
 export default {
-  QQ_CLIENT_ID: "101995894",
+  QQ_CLIENT_ID: "243455",
   QQ_REDIRECT_URL: "http://www.codescholar.cn/callback/qq",
   GITEE_CLIENT_ID: "d029020a6839273e0e82d853783874bf1f4f34b95da6ebcbc230e6603ff9b656",
   GITEE_REDIRECT_URL: "http://www.codescholar.cn/callback/gitee",
