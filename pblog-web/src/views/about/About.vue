271561e0ca4d5d62107ba4c320ff755dd5044751
codescholar <1055826438@qq.com>
2023-08-23 22:53:10 +0800

    优化代码高亮

--- pblog-web/src/views/about/About.vue
+++ pblog-web/src/views/about/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <Loading></Loading>
     <!-- banner -->
     <div class="banner" :style="cover">
       <div class="bgShade">
@@ -28,8 +29,13 @@
 
 <script>
   import Clipboard from "clipboard";
+  import hljs from "../../plugins/hightLight/index";
+  import Loading from "@/components/loading/loading";
 
   export default {
+    components: {
+      Loading
+    },
     created() {
       this.getAboutContent();
     },
@@ -76,7 +82,6 @@
       },
       markdownToHtml(data) {
         const MarkdownIt = require("markdown-it");
-        const hljs = require("highlight.js");
         const md = new MarkdownIt({
           html: true,
           linkify: true,
@@ -174,102 +179,3 @@
     }
 </style>
 
-<style lang="scss">
-  pre.hljs {
-    padding: 12px 2px 12px 40px !important;
-    border-radius: 5px !important;
-    position: relative;
-    font-size: 16px !important;
-    line-height: 22px !important;
-    overflow: hidden !important;
-
-    &:hover .copy-btn {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-    }
-
-    code {
-      display: block !important;
-      margin: 0 10px !important;
-      overflow-x: auto !important;
-
-      &::-webkit-scrollbar {
-        z-index: 11;
-        width: 6px;
-      }
-
-      &::-webkit-scrollbar:horizontal {
-        height: 6px;
-      }
-
-      &::-webkit-scrollbar-thumb {
-        border-radius: 5px;
-        width: 6px;
-        background: #666;
-      }
-
-      &::-webkit-scrollbar-corner,
-      &::-webkit-scrollbar-track {
-        background: #1e1e1e;
-      }
-
-      &::-webkit-scrollbar-track-piece {
-        background: #1e1e1e;
-        width: 6px;
-      }
-    }
-
-    .line-numbers-rows {
-      position: absolute;
-      pointer-events: none;
-      top: 12px;
-      bottom: 12px;
-      left: 0;
-      font-size: 100%;
-      width: 40px;
-      text-align: center;
-      letter-spacing: -1px;
-      border-right: 1px solid rgba(0, 0, 0, 0.66);
-      user-select: none;
-      counter-reset: linenumber;
-
-      span {
-        pointer-events: none;
-        display: block;
-        counter-increment: linenumber;
-
-        &:before {
-          content: counter(linenumber);
-          color: #999;
-          display: block;
-          text-align: center;
-        }
-      }
-    }
-
-    b.name {
-      position: absolute;
-      top: 7px;
-      right: 45px;
-      z-index: 1;
-      color: #999;
-      pointer-events: none;
-    }
-
-    .copy-btn {
-      position: absolute;
-      top: 6px;
-      right: 6px;
-      z-index: 1;
-      color: #ccc;
-      background-color: #525252;
-      border-radius: 6px;
-      display: none;
-      font-size: 16px;
-      width: 32px;
-      height: 24px;
-      outline: none;
-    }
-  }
-</style>
