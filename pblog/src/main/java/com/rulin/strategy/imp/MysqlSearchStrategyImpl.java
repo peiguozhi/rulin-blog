4cff128bf1b7de13f46067b7e97024b23dd62aa1
codescholar <1055826438@qq.com>
2023-08-23 21:13:23 +0800

    修改文章和面试题功能的搜索功能，将文章内容搜索改为文章简介搜索

--- pblog/src/main/java/com/rulin/strategy/imp/MysqlSearchStrategyImpl.java
+++ pblog/src/main/java/com/rulin/strategy/imp/MysqlSearchStrategyImpl.java
@@ -8,6 +8,7 @@ import com.rulin.entity.Question;
 import com.rulin.mapper.ArticleMapper;
 import com.rulin.mapper.QuestionMapper;
 import com.rulin.strategy.SearchStrategy;
+import com.rulin.util.StringUtils;
 import com.rulin.vo.ArticleSearchVO;
 import com.rulin.vo.QuestionSearchVO;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +16,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 import static com.rulin.enums.PublishEnum.PUBLISH;
@@ -34,48 +37,55 @@ public class MysqlSearchStrategyImpl implements SearchStrategy {
                 .eq(BlogArticle::getIsPublish, PUBLISH.getCode())
                 .and(i -> i.like(BlogArticle::getTitle, keywords)
                         .or()
-                        .like(BlogArticle::getContent, keywords)).orderByDesc(BlogArticle::getIsStick,BlogArticle::getCreateTime));
+                        .like(BlogArticle::getSummary, keywords)).orderByDesc(BlogArticle::getIsStick,BlogArticle::getCreateTime));
         //高亮处理
         return blogArticles.stream().map(item -> {
             // 获取关键词第一次出现的位置
-            String articleContent = item.getContent();
-            int index = item.getContent().indexOf(keywords);
+            String articleSummary = StringUtils.replaceKeywords(item.getSummary(), keywords);
+/*            int index = item.getSummary().indexOf(keywords);
             if (index != -1) {
                 // 获取关键词前面的文字
                 int preIndex = index > 25 ? index - 25 : 0;
-                String preText = item.getContent().substring(preIndex, index);
+                String preText = item.getSummary().substring(preIndex, index);
                 // 获取关键词到后面的文字
                 int last = index + keywords.length();
-                int postLength = item.getContent().length() - last;
+                int postLength = item.getSummary().length() - last;
                 int postIndex = postLength > 175 ? last + 175 : last + postLength;
-                String postText = item.getContent().substring(index, postIndex);
+                String postText = item.getSummary().substring(index, postIndex);
                 // 文章内容高亮
-                articleContent = (preText + postText).replaceAll(keywords, Constants.PRE_TAG + keywords + Constants.POST_TAG);
-            }
+                articleSummary = (preText + postText).replaceAll(keywords, Constants.PRE_TAG + keywords + Constants.POST_TAG);
+            }*/
             // 文章标题高亮
-            String articleTitle = item.getTitle().replaceAll(keywords, Constants.PRE_TAG + keywords + Constants.POST_TAG);
+            String articleTitle = StringUtils.replaceKeywords(item.getTitle(), keywords);
             return ArticleSearchVO.builder()
                     .id(item.getId())
                     .title(articleTitle)
-                    .content(articleContent)
+                    .content(articleSummary)
                     .build();
         }).collect(Collectors.toList());
     }
 
     @Override
     public List<QuestionSearchVO> searchQuestion(String keywords) {
-        // 搜索文章
+        // 搜索面试题
         List<Question> question = questionMapper.selectList(new LambdaQueryWrapper<Question>()
                 .eq(Question::getIsPublish, PUBLISH.getCode())
                 .and(i -> i.like(Question::getQuContent, keywords)).orderByDesc(Question::getSort,Question::getCreateTime));
         //高亮处理
         return question.stream().map(item -> {
+            String quContent = item.getQuContent();
             // 文章标题高亮
-            String quContent = item.getQuContent().replaceAll("(?i)"+keywords, Constants.PRE_TAG + keywords + Constants.POST_TAG);
+            Matcher matcher = Pattern.compile(keywords, Pattern.CASE_INSENSITIVE).matcher(quContent);
+            // 查找匹配的内容
+            while (matcher.find()) {
+                // 打印找到的内容
+                quContent = quContent.replaceAll("(?i)"+matcher.group(), Constants.PRE_TAG + matcher.group() + Constants.POST_TAG);
+            }
+
             return QuestionSearchVO.builder()
                     .id(item.getId())
                     .quContent(quContent)
-                    .analysis(item.getAnalysis())
+                    .analysis(StringUtils.removeLabel(item.getAnalysis()))
                     .qCategoryId(item.getQCategoryId())
                     .build();
         }).collect(Collectors.toList());
