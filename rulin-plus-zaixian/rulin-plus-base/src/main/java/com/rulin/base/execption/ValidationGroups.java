018bd81dc118bdefd4d928d98218619dd3889b57
codescholar <1055826438@qq.com>
2023-08-27 21:53:14 +0800

    no message

---
+++ rulin-plus-zaixian/rulin-plus-base/src/main/java/com/rulin/base/execption/ValidationGroups.java
@@ -0,0 +1,13 @@
+package com.rulin.base.execption;
+
+/**
+ * 校验分组
+ *
+ * @author 程序儒
+ * @date 2023-08-15 14:55:20
+ */
+public class ValidationGroups {
+    public interface Inster {};
+    public interface Update {};
+    public interface Delete {};
+}
