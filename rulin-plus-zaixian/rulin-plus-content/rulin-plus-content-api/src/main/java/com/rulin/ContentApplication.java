42aac8be3e4250da6d3db1dad034a16b737e4882
codescholar <1055826438@qq.com>
2023-08-28 23:01:57 +0800

    no message

---
+++ rulin-plus-zaixian/rulin-plus-content/rulin-plus-content-api/src/main/java/com/rulin/ContentApplication.java
@@ -0,0 +1,19 @@
+package com.rulin;
+
+import com.spring4all.swagger.EnableSwagger2Doc;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 程序儒
+ * @version 1.0.0
+ * @date 2023/8/11
+ * @apiNote
+ */
+@SpringBootApplication
+@EnableSwagger2Doc
+public class ContentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ContentApplication.class, args);
+    }
+}
