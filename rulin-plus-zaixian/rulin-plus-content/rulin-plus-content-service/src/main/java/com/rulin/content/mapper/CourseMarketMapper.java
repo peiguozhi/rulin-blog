e15718c25bc94156b491cb2449b5cb8d6cf1521c
codescholar <1055826438@qq.com>
2023-09-06 13:06:43 +0800

    无用

---
+++ rulin-plus-zaixian/rulin-plus-content/rulin-plus-content-service/src/main/java/com/rulin/content/mapper/CourseMarketMapper.java
@@ -0,0 +1,15 @@
+package com.rulin.content.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.rulin.content.model.po.CourseMarket;
+
+/**
+ * <p>
+ * 课程营销信息 Mapper 接口
+ * </p>
+ *
+ * @author 程序儒
+ */
+public interface CourseMarketMapper extends BaseMapper<CourseMarket> {
+
+}
