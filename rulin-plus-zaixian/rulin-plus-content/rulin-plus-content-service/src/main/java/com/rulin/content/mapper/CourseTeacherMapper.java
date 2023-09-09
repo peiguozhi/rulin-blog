fd6a9a2acc9e4181bacc4145d90214de563e42a4
codescholar <1055826438@qq.com>
2023-09-09 13:07:14 +0800

    no message

---
+++ rulin-plus-zaixian/rulin-plus-content/rulin-plus-content-service/src/main/java/com/rulin/content/mapper/CourseTeacherMapper.java
@@ -0,0 +1,15 @@
+package com.rulin.content.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.rulin.content.model.po.CourseTeacher;
+
+/**
+ * <p>
+ * 课程-教师关系表 Mapper 接口
+ * </p>
+ *
+ * @author 程序儒
+ */
+public interface CourseTeacherMapper extends BaseMapper<CourseTeacher> {
+
+}
