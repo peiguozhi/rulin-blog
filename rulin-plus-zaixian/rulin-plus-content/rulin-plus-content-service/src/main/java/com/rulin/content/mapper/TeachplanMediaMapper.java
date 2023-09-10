cca28e70d6c44382857ad0e0c1668ae35e091e66
codescholar <1055826438@qq.com>
2023-09-10 21:45:23 +0800

    no message

---
+++ rulin-plus-zaixian/rulin-plus-content/rulin-plus-content-service/src/main/java/com/rulin/content/mapper/TeachplanMediaMapper.java
@@ -0,0 +1,15 @@
+package com.rulin.content.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.rulin.content.model.po.TeachplanMedia;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author 程序儒
+ */
+public interface TeachplanMediaMapper extends BaseMapper<TeachplanMedia> {
+
+}
