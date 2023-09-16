3083d09b15cbf2f527e4d3d5ec70c77bdcc1cda8
codescholar <1055826438@qq.com>
2023-09-16 12:57:33 +0800

    no message

---
+++ rulin-plus-zaixian/rulin-plus-content/rulin-plus-content-service/src/test/java/com/rulin/content/CourseBaseInfoServiceTests.java
@@ -0,0 +1,38 @@
+package com.rulin.content;
+
+
+
+import com.rulin.base.model.PageParams;
+import com.rulin.base.model.PageResult;
+import com.rulin.content.model.dto.QueryCourseParamsDto;
+import com.rulin.content.model.po.CourseBase;
+import com.rulin.content.service.CourseBaseInfoService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseBaseInfoServiceTests {
+
+    @Autowired
+    CourseBaseInfoService courseBaseInfoService;
+
+
+    @Test
+    void testCourseBaseInfoService() {
+        //查询条件
+        QueryCourseParamsDto queryCourseParamsDto = new QueryCourseParamsDto();
+        queryCourseParamsDto.setCourseName("java");
+        queryCourseParamsDto.setAuditStatus("202004");
+        queryCourseParamsDto.setPublishStatus("203001");
+
+        //分页参数
+        PageParams pageParams = new PageParams();
+        pageParams.setPageNo(1L);//页码
+        pageParams.setPageSize(3L);//每页记录数
+
+        PageResult<CourseBase> courseBasePageResult = courseBaseInfoService.queryCourseBaseList(pageParams, queryCourseParamsDto);
+        System.out.println(courseBasePageResult);
+    }
+
+}
