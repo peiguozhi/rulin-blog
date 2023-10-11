be489b030290693e7481b311113e3b60296a60da
codescholar <1055826438@qq.com>
2023-10-11 18:48:08 +0800

    no message

---
+++ rulin-plus-zaixian/rulin-plus-geteway/src/main/java/com/rulin/GetewayApplication.java
@@ -0,0 +1,13 @@
+package com.rulin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GetewayApplication.class, args);
+    }
+
+}
