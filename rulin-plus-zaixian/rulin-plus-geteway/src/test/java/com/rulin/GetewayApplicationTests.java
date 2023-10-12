4053347e617d6cc9df747fb644f96650aeca4670
codescholar <1055826438@qq.com>
2023-10-12 22:53:47 +0800

    no message

---
+++ rulin-plus-zaixian/rulin-plus-geteway/src/test/java/com/rulin/GetewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rulin;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
