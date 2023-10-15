122e20a5a8258297ff4650c9fde9a7e5d8a83aeb
codescholar <1055826438@qq.com>
2023-10-15 21:05:06 +0800

    no message

---
+++ rulin-plus-zaixian/rulin-plus-media/rulin-plus-media-api/src/main/java/com/rulin/media/api/BigFilesController.java
@@ -0,0 +1,80 @@
+package com.rulin.media.api;
+
+
+import com.rulin.base.model.RestResponse;
+import com.rulin.media.model.dto.UploadFileParamsDto;
+import com.rulin.media.service.MediaFileService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+
+
+/**
+ * 大文件上传接口
+ *
+ * @author 程序儒
+ * @date 2023/08/27 01:16:15
+ */
+@Api(value = "大文件上传接口", tags = "大文件上传接口")
+@RestController
+public class BigFilesController {
+
+    @Autowired
+    private MediaFileService mediaFileService;
+
+    @ApiOperation(value = "文件上传前检查文件")
+    @PostMapping("/upload/checkfile")
+    public RestResponse<Boolean> checkfile(
+            @RequestParam("fileMd5") String fileMd5
+    ) throws Exception {
+        return mediaFileService.checkFile(fileMd5);
+    }
+
+
+    @ApiOperation(value = "分块文件上传前的检测")
+    @PostMapping("/upload/checkchunk")
+    public RestResponse<Boolean> checkchunk(@RequestParam("fileMd5") String fileMd5,
+                                            @RequestParam("chunk") int chunk) throws Exception {
+        return mediaFileService.checkChunk(fileMd5,chunk);
+    }
+
+    @ApiOperation(value = "上传分块文件")
+    @PostMapping("/upload/uploadchunk")
+    public RestResponse uploadchunk(@RequestParam("file") MultipartFile file,
+                                    @RequestParam("fileMd5") String fileMd5,
+                                    @RequestParam("chunk") int chunk) throws Exception {
+
+        //创建临时文件
+        File tempFile = File.createTempFile("minio", "temp");
+        //上传的文件拷贝到临时文件
+        file.transferTo(tempFile);
+        //文件路径
+        String absolutePath = tempFile.getAbsolutePath();
+        return mediaFileService.uploadChunk(fileMd5,chunk,absolutePath);
+
+    }
+
+    @ApiOperation(value = "合并文件")
+    @PostMapping("/upload/mergechunks")
+    public RestResponse mergechunks(@RequestParam("fileMd5") String fileMd5,
+                                    @RequestParam("fileName") String fileName,
+                                    @RequestParam("chunkTotal") int chunkTotal) throws Exception {
+        Long companyId = 1232141425L;
+
+        UploadFileParamsDto uploadFileParamsDto = new UploadFileParamsDto();
+        uploadFileParamsDto.setFileType("001002");
+        uploadFileParamsDto.setTags("课程视频");
+        uploadFileParamsDto.setRemark("");
+        uploadFileParamsDto.setFilename(fileName);
+
+        return mediaFileService.mergechunks(companyId,fileMd5,chunkTotal,uploadFileParamsDto);
+    }
+
+
+}
