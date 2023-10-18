4fa7c03f0f29c8de72c50900c1c3f1189e20195b
codescholar <1055826438@qq.com>
2023-10-18 21:37:00 +0800

    no message

---
+++ rulin-plus-zaixian/rulin-plus-media/rulin-plus-media-model/src/main/java/com/rulin/media/model/dto/UploadFileResultDto.java
@@ -0,0 +1,18 @@
+package com.rulin.media.model.dto;
+
+
+import com.rulin.media.model.po.MediaFiles;
+import lombok.Data;
+
+
+/**
+ * 上传普通文件成功响应结果
+ *
+ * @author 程序儒
+ * @date 2023-08-26 16:53:57
+ */
+@Data
+public class UploadFileResultDto extends MediaFiles {
+  
+
+}
