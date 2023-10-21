0a9a9c474861117a7af981b989d11bdc5492e1ec
codescholar <1055826438@qq.com>
2023-10-21 22:23:37 +0800

    no message

---
+++ rulin-plus-zaixian/rulin-plus-media/rulin-plus-media-service/src/main/java/com/rulin/media/config/MybatisPlusConfig.java
@@ -0,0 +1,35 @@
+package com.rulin.media.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+/**
+ * <p>
+ * Mybatis-Plus 配置
+ * </p>
+ *
+ * @author 程序儒
+ * @date 2023-08-25 18:26:59
+ */
+@Configuration
+@MapperScan("com.rulin.media.mapper")
+public class MybatisPlusConfig {
+	/**
+	 * 新的分页插件
+	 * 需要设置 MybatisConfiguration#useDeprecatedExecutor = false
+	 * 避免缓存出现问题(该属性会在旧插件移除后一同移除)
+	 */
+	@Bean
+	public MybatisPlusInterceptor mybatisPlusInterceptor() {
+		MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+		interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+		return interceptor;
+	}
+
+
+}
