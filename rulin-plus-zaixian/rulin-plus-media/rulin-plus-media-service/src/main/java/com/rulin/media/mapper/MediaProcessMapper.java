aa1938d95f0537df3d8249f02726eda935b73d8b
codescholar <1055826438@qq.com>
2023-10-23 21:21:05 +0800

    no message

---
+++ rulin-plus-zaixian/rulin-plus-media/rulin-plus-media-service/src/main/java/com/rulin/media/mapper/MediaProcessMapper.java
@@ -0,0 +1,16 @@
+package com.rulin.media.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.rulin.media.model.po.MediaProcess;
+
+/**
+ * <p>
+ * Mapper 接口
+ * </p>
+ *
+ * @author 程序儒
+ * @date 2023-08-25 18:27:47
+ */
+public interface MediaProcessMapper extends BaseMapper<MediaProcess> {
+
+}
