6f0b3aecba76106e23c1864201dbd71adad43398
codescholar <1055826438@qq.com>
2023-10-24 22:40:38 +0800

    no message

---
+++ rulin-plus-zaixian/rulin-plus-media/rulin-plus-media-service/src/main/java/com/rulin/media/mapper/MqMessageMapper.java
@@ -0,0 +1,16 @@
+package com.rulin.media.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.rulin.media.model.po.MqMessage;
+
+/**
+ * <p>
+ * Mapper 接口
+ * </p>
+ *
+ * @author 程序儒
+ * @date 2023-08-25 18:28:02
+ */
+public interface MqMessageMapper extends BaseMapper<MqMessage> {
+
+}
