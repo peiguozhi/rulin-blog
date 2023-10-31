c5919c5ccaa1184688e779897fbbf8266f55efa6
codescholar <1055826438@qq.com>
2023-11-01 00:00:00 +0800

    no message

---
+++ rulin-plus-zaixian/rulin-plus-system/rulin-plus-system-model/src/main/java/com/rulin/system/model/po/Dictionary.java
@@ -0,0 +1,58 @@
+package com.rulin.system.model.po;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.io.Serializable;
+
+
+/**
+ * 数据字典
+ *
+ * @author 程序儒
+ * @date 2023-08-14 13:26:18
+ */
+@Data
+@TableName("dictionary")
+public class Dictionary implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * id标识
+     */
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+
+    /**
+     * 数据字典名称
+     */
+    private String name;
+
+    /**
+     * 数据字典代码
+     */
+    private String code;
+
+    /**
+     * 数据字典项--json格式
+            [{
+                  "sd_name": "低级",
+                  "sd_id": "200001",
+                  "sd_status": "1"
+               }, {
+                  "sd_name": "中级",
+                  "sd_id": "200002",
+                  "sd_status": "1"
+               }, {
+                  "sd_name": "高级",
+                  "sd_id": "200003",
+                  "sd_status": "1"
+               }]
+     */
+    private String itemValues;
+
+
+}
