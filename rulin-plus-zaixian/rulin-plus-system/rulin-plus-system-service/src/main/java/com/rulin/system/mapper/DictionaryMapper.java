ab6ba760fcd01751130b6abb555aff0e9b452ad1
codescholar <1055826438@qq.com>
2023-11-01 23:17:53 +0800

    no message

---
+++ rulin-plus-zaixian/rulin-plus-system/rulin-plus-system-service/src/main/java/com/rulin/system/mapper/DictionaryMapper.java
@@ -0,0 +1,15 @@
+package com.rulin.system.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.rulin.system.model.po.Dictionary;
+
+
+/**
+ * 数据字典 Mapper 接口
+ *
+ * @author 程序儒
+ * @date 2023-08-14 13:26:53
+ */
+public interface DictionaryMapper extends BaseMapper<Dictionary> {
+
+}
