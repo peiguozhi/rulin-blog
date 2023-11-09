0978370d0a9b2272a99d634ee1ace03da0ebb22e
codescholar <1055826438@qq.com>
2023-11-09 21:15:48 +0800

    no message

---
+++ rulin-plus-zaixian/xuecheng-plus-auth/src/main/java/com/xuecheng/auth/controller/WxLoginController.java
@@ -0,0 +1,37 @@
+package com.xuecheng.auth.controller;
+
+import com.xuecheng.ucenter.model.po.XcUser;
+import com.xuecheng.ucenter.service.WxAuthService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.IOException;
+
+/**
+ * @author Mr.M
+ * @version 1.0
+ * @description TODO
+ * @date 2023/2/24 15:13
+ */
+@Slf4j
+@Controller
+public class WxLoginController {
+
+    @Autowired
+    WxAuthService wxAuthService;
+
+    @RequestMapping("/wxLogin")
+    public String wxLogin(String code, String state) throws IOException {
+        log.debug("微信扫码回调,code:{},state:{}", code, state);
+        //远程调用微信请令牌，拿到令牌查询用户信息，将用户信息写入本项目数据库
+        XcUser xcUser = wxAuthService.wxAuth(code);
+
+        if (xcUser == null) {
+            return "redirect:http://www.51xuecheng.cn/error.html";
+        }
+        String username = xcUser.getUsername();
+        return "redirect:http://www.51xuecheng.cn/sign.html?username=" + username + "&authType=wx";
+    }
+}
